6f964aa80f8505785c4824d0c56b8c5290c9a7cb
guoguo <guowei_leo@aliyun.com>
2024-03-04 16:57:33 +0800

    Add files via upload
    添加libs文件夹

---
+++ libs/common/src/database/database.providers.ts
@@ -0,0 +1,52 @@
+import { DataSource } from "typeorm";
+import { getConfig } from "../utils";
+import { NamingStrategy } from "./naming.strategies";
+import * as path from 'path';
+
+const { MONGODB_CONFIG, MYSQL_CONFIG } = getConfig();
+
+const MONGODB_DATABASE_CONFIG = {
+  ...MONGODB_CONFIG,
+  NamedNodeMap: new NamingStrategy(),
+  entities: [
+    path.join(
+    __dirname,
+      // `../../../../**/*.${MONGODB_CONFIG.entities}.entity.ts`,
+      `dist/**/*.${MONGODB_CONFIG.entities}.entity.ts`,
+    ),
+  ],
+};
+
+const MYSQL_DATABASE_CONFIG = {
+  ...MYSQL_CONFIG,
+  namingStrategy: new NamingStrategy(),
+  entities: [
+    path.join(
+      __dirname,
+      `dist/**/*.${MYSQL_CONFIG.entities}.entity.ts`,
+    ),
+  ],
+};
+
+const MONGODB_DATA_SOURCE = new DataSource(MONGODB_DATABASE_CONFIG);
+const MYSQL_DATA_SOURCE = new DataSource(MYSQL_DATABASE_CONFIG);
+
+//数据库注入 
+export const DatabaseProviders = [
+  {
+    provide: 'MONGODB_DATA_SOURCE',
+    useFactory: async () => {
+      if (!MONGODB_DATA_SOURCE.isInitialized)
+        await MONGODB_DATA_SOURCE.initialize();
+      return MONGODB_DATA_SOURCE;
+    },
+  },
+  {
+    provide: 'MYSQL_DATA_SOURCE',
+    useFactory: async () => {
+      if (!MYSQL_DATA_SOURCE.isInitialized)
+        await MYSQL_DATA_SOURCE.initialize();
+      return MYSQL_DATA_SOURCE;
+    }
+  }
+]
